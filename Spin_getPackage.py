fbd2cc12dc5098b796497ef5b817ab8bf7950544
Lork Scorguar <lorkscorguar@gmail.com>
2016-04-15 15:13:17 +0200

    Reorder files

--- Spin_getPackage.py
+++
@@ -1,9 +0,0 @@
-#!/opt/opsware/bin/python2
-#simple sample on how to get a package from spin
-
-import sys
-sys.path.append("/opt/opsware/pylibs2")
-from coglib import spinwrapper
-pid=input("Enter package id:")
-spin = spinwrapper.SpinWrapper("http://localhost:1007")
-spin.Unit.get(id=pid)
