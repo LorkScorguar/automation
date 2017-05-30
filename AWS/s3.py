a9fd472a9d2e62c7b883c91330887114c711a789
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-05-30 11:53:22 +0200

    add function to list s3 objects

---
+++ AWS/s3.py
@@ -0,0 +1,26 @@
+"""
+Module to manipulate ec2 resources
+"""
+import datetime
+import re
+import boto3
+import secret
+
+ACCESS_KEY_ID, SECRET_ACCESS_KEY = secret.getAccess()
+REGION = secret.getRegion()
+
+S3C = boto3.client(service_name='s3', aws_access_key_id=ACCESS_KEY_ID,
+                    aws_secret_access_key=SECRET_ACCESS_KEY, region_name=REGION)
+
+
+DRY = True
+
+def listObjects(verbose, bucket):
+    jResp = S3C.list_objects(Bucket=bucket)
+    res = {}
+    for item in jResp['Contents']:
+        if verbose:
+            res[item['Key']]+";"+str(item['Size'])+";"+str(item['LastModified'])
+        else:
+            res[item['Key']]+";"+str(item['Size'])
+    return res
