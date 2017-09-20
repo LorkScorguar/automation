c70e9845d070f80d4a3400da430b2e45e07b7225
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-09-20 11:01:14 +0200

    improve lint add a tasks queue

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -8,6 +8,7 @@ Reccurents tasks:
 import base64
 import datetime
 import os
+import re
 import sys
 import threading
 import time
@@ -17,47 +18,41 @@ sys.path.append("../ManageIQ")
 import MIQ
 import Config
 
+authValue = "Basic "+base64.b64encode(bytes(Config.cluser+":"+Config.clpassword,'utf-8')).decode('utf-8')
+tasks = {}
+
 def updateConfig(param,value):
     #save configuration
-    save={}
-    cfg=open("Config.py","r")
+    save = {}
+    cfg = open("Config.py","r")
     for line in cfg:
-        parm=line.strip().split("=")[0]
-        val=line.strip().split("=")[1]
-        save[parm]=val
+        parm = line.strip().split("=")[0]
+        val  line.strip().split("=")[1]
+        save[parm] = val
     cfg.close()
     #do the change and write file
-    save[param]='"'+value+'"'
-    cfg=open("Config.py","w")
+    save[param] = '"'+value+'"'
+    cfg = open("Config.py","w")
     for k,v in save.items():
         cfg.write(str(k)+"="+str(v)+"\n")
     cfg.close()
 
 def setProxy():
-    os.environ['http_proxy']=Config.http_proxy
-    os.environ['https_proxy']=Config.https_proxy
-    os.environ['HTTP_PROXY']=Config.http_proxy
-    os.environ['HTTPS_PROXY']=Config.https_proxy
+    os.environ['http_proxy'] = Config.http_proxy
+    os.environ['https_proxy'] = Config.https_proxy
+    os.environ['HTTP_PROXY'] = Config.http_proxy
+    os.environ['HTTPS_PROXY'] = Config.https_proxy
 
 def checkMIQ(authValue,clconfig):
-    url=clconfig['url']
-    platform="Miq "+clconfig['name']
-    nb,res=MIQ.checkPendingRequests(authValue,url)
+    url = clconfig['url']
+    platform = "Miq "+clconfig['name']
+    nb, res = MIQ.checkPendingRequests(authValue,url)
     if nb > 0:
-        title="["+platform+"] Awaiting Validations: "+str(nb)
+        title = "["+platform+"] Awaiting Validations: "+str(nb)
         print(title)
         print(res)
-        print("\n")
-        while nb > 0:
-            choice = input("Enter action to do (d or a +requestID): ")
-            requestID = choice[1:]
-            if choice[0] == "d":
-                action = "deny"
-            elif choice[0] == "a":
-                action = "approve"
-            resp = MIQ.updateApproval(authValue,url,requestID,action)
-            print(resp)
-            nb-=1
+        for task in res.split("\n"):
+            tasks[task.split(":")[0]] = url
     else:
         print("["+platform+"] Nothing awaiting you")
 
@@ -68,9 +63,9 @@ def checkAWS(user):
     for k,v in dres.items():
         for ac in v:
             if ac['createdDate']<days90:
-                age=(today-ac['createdDate']).days
+                age = (today-ac['createdDate']).days
                 print("Access key "+ac['accessKeyId']+" for user "+k+" is old ("+str(age)+" days)")
-                ak,sak=iam.generateKey(False,user)
+                ak, sak = iam.generateKey(False,user)
                 updateConfig("aws_access_key",ak)
                 updateConfig("aws_secret_key",sak)
                 iam.deleteKey(False,user,Config.aws_access_key)
@@ -82,30 +77,42 @@ def recurring(id, e, stop):
         #AWS
         #checkAWS(Config.aws_user)
         #ManageIQ
-        authValue="Basic "+base64.b64encode(bytes(Config.cluser+":"+Config.clpassword,'utf-8')).decode('utf-8')
         checkMIQ(authValue,Config.clurlppr)
         checkMIQ(authValue,Config.clurlprd)
         e.wait(timeout=600)
 
-
+def analyse(inp):
+    """
+    function to analyse user input and get the best answer
+    """
+    if re.search("^(a|d)[0-9]*$",inp):
+        if inp[0] == "d":
+            action = "deny"
+        elif inp[0] == "a":
+            action = "approve"
+        requestID = inp[1:]
+        url = tasks[requestID]
+        resp = MIQ.updateApproval(authValue,url,requestID,action)
+        del tasks[requestID]
+        print(resp)
 
 if __name__ == '__main__':
     print("Welcome")
     quitter=False
-    e=threading.Event()
-    a=threading.Thread(None,recurring,"recurring",args=(id, e, lambda: quitter))
+    e = threading.Event()
+    a = threading.Thread(None,recurring,"recurring",args=(id, e, lambda: quitter))
     a.start()
     while not quitter:
-        inp=input(">")
-        if inp=="quit":
-            quitter=True
+        inp = input(">")
+        if inp == "quit":
+            quitter = True
             e.set()
             a.join()
         else:
             try:
-                res=eval(inp)
+                res = eval(inp)
             except:
-                if inp!="":
+                if inp != "":
                     res="Received: "+str(inp)
         print(res)
     print("Bye")
