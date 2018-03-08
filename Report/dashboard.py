aebab1525e7958810ee2dc48ea523b70c7170db7
LorkScorguarm <lorkscorguar@gmail.com>
2018-03-08 18:45:15 +0100

    separate login page

--- Report/dashboard.py
+++ Report/dashboard.py
@@ -1,38 +1,22 @@
 from flask import Flask, session, redirect, url_for, escape, request
+from login import login_page
 
 app = Flask(__name__)
+app.register_blueprint(login_page)
 
 @app.route('/')
 def index():
     if 'username' in session:
         return 'Logged in as %s' % escape(session['username'])
     else:
-        redirect(url_for('login'))
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    login_form='''
-        <form method="post">
-            <p>Username:<input type=text name=username></p>
-            <p>Password:<input type=secret name=password></p>
-            <p><input type=submit value=Login></p>
-        </form>
-    '''
-    if request.method == 'POST':
-        if request.form['username']=="test" and request.form['password']=="test":
-            session['username'] = request.form['username']
-            session['password'] = request.form['password']
-            return redirect(url_for('index'))
-        else:
-            login_form="<p>invalid credentials</p>"+login_form
-    return login_form
+        redirect(url_for('login_page.login'))
 
 @app.route('/logout')
 def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
     session.pop('password', None)
-    return redirect(url_for('index'))
+    return redirect(url_for('login_page.login'))
 
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
