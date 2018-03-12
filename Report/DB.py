00849ccb16fa9fc889572c6b007a3b482e1e34fc
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2018-03-12 09:46:24 +0100

    correct ID

--- Report/DB.py
+++ Report/DB.py
@@ -2,7 +2,7 @@ import csv
 
 def getAllRun():
     allRun={}
-    reader=csv.DictReader((open("database/allRun.csv"))
+    reader=csv.DictReader((open("database/allRun.csv")))
     for row in reader:
         allRun[row['id']]={"uuid":row['uuid'],"name":row['name'],"duration":row['duration'],"status":row['status'],"user":row['user'],"message":row['message']}
     return allRun
