36fd534fb3eea3c4fdfa27e70942a9a6ec6654fa
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2018-03-12 10:03:38 +0100

    use orderedDict

--- Report/DB.py
+++ Report/DB.py
@@ -1,7 +1,8 @@
 import csv
+from collections import OrderedDict
 
 def getAllRun():
-    allRun={}
+    allRun=OrderedDict()
     reader=csv.DictReader((open("database/allRun.csv")))
     for row in reader:
         allRun[row['id']]={"uuid":row['uuid'],"name":row['name'],"duration":row['duration'],"status":row['status'],"user":row['user'],"message":row['message']}
