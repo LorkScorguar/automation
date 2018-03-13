da5ecab0a5f46296c13ed1fe664a15078c6b8283
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2018-03-13 16:49:17 +0100

    use a template for login page

--- Report/login.py
+++ Report/login.py
@@ -1,17 +1,11 @@
-from flask import Flask, Blueprint, session, redirect, url_for, escape, request
+from flask import Flask, Blueprint, session, redirect, render_template, url_for, escape, request
 import DB
 
 login_page=Blueprint("login_page",__name__)
 @login_page.route('/login', methods=['GET', 'POST'])
 
 def login():
-    login_form='''
-        <form method="post">
-            <p>Username:<input type=text name=username></p>
-            <p>Password:<input type=secret name=password></p>
-            <p><input type=submit value=Login></p>
-        </form>
-    '''
+    invalid=False
     if request.method == 'POST':
         if request.form['username']=="admin" and request.form['password']=="admin":
             session['username'] = request.form['username']
@@ -20,5 +14,5 @@ def login():
             session['logged_in'] = True
             return redirect(url_for('index'))
         else:
-            login_form="<p>invalid credentials</p>"+login_form
-    return login_form
+            invalid=True
+    return render_template('login.html',invalid=invalid)
